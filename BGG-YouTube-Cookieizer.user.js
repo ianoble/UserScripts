f9a9ceb0fc8c3151def17a81997cc03707240e0c
Brian Baker <brian@holisticdeveloper.com>
2018-04-08 16:12:24 -0400

    Create BGG-YouTube-Cookieizer.user.js

---
+++ BGG-YouTube-Cookieizer.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         BGG YouTube Cookieizer
+// @namespace    https://github.com/HolisticDeveloper/UserScripts
+// @version      0.1
+// @description  Enables YouTube cookies on BoardGameGeek (to get the Watch Later button)
+// @author       HolisticDeveloper
+// @match        https://www.boardgamegeek.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+var xpath = `(
+	//iframe[
+		contains(@src, 'youtube-nocookie.com/embed/')
+	]
+)[not(ancestor::*[@id='YTLT-player'])]`;
+
+var addCookies = function(node){
+
+	var result = document.evaluate(
+		xpath, node, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+
+	var element = null;
+	var i = 0, j;
+
+	while ((element = result.snapshotItem(i++))) {
+        element.src = element.src.replace("youtube-nocookie.com", "youtube.com");
+	}
+};
+
+new MutationObserver(function(){
+	if (document.body) {
+		addCookies(document.body);
+	}
+}).observe(document, {
+	childList: true,
+	subtree: true
+});
+
+})();
+
